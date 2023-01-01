56cf19e29ecb1148cef0efdf2e9fd8d4171d7c8b
Documenter.jl <documenter@juliadocs.github.io>
2023-01-01 19:19:28 +0000

    build based on 07aaf50

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KhepriIFC","category":"page"},{"location":"#KhepriIFC","page":"Home","title":"KhepriIFC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KhepriIFC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KhepriIFC]","category":"page"}]
+}
